8f88291bae464b45b157283249df892764d80dfa
朱雨桐 <15127099870@163.com>
2016-09-20 15:21:00 +0800

    model文件夹

--- app/src/main/java/com/qf/tgp/plamtv/constants/HttpConstants.java
+++ app/src/main/java/com/qf/tgp/plamtv/constants/HttpConstants.java
@@ -5,5 +5,6 @@ package com.qf.tgp.plamtv.constants;
  */
 public class HttpConstants {
 
-
+    // 栏目页面网址
+    public static String COLUMN_URL = "http://www.quanmin.tv/json/categories/list.json?09191932&device=355066062641818&v=2.1.3&screen=2&ch=OT_wdjcpd&sh=1920&sw=1080&uid=a219b8aa&net=0&ver=4&os=1";
 }
--- app/src/main/java/com/qf/tgp/plamtv/ui/fragments/ColumnFragment.java
+++ app/src/main/java/com/qf/tgp/plamtv/ui/fragments/ColumnFragment.java
@@ -2,13 +2,18 @@ package com.qf.tgp.plamtv.ui.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.qf.tgp.plamtv.BaseFragment;
 import com.qf.tgp.plamtv.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by 朱雨桐 on 2016/9/19.
@@ -16,11 +21,26 @@ import com.qf.tgp.plamtv.R;
 public class ColumnFragment extends BaseFragment {
 
     public static final String TAG = ColumnFragment.class.getSimpleName();
+    @BindView(R.id.column_search)
+    ImageView mSearch;
+    @BindView(R.id.column_recycler_view)
+    RecyclerView mRecyclerView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        layout = inflater.inflate(R.layout.fragment_column,container,false);
+        layout = inflater.inflate(R.layout.fragment_column, container, false);
+        ButterKnife.bind(this, layout);
         return layout;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // 设置RecyclerView的布局管理器
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 3);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+
     }
 }
