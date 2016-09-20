b4c1ca3821efc13bd81751287394f41875ab4e56
Peng <83474571@qq.com>
2016-09-20 11:54:14 +0800

    commit

--- app/src/main/java/com/qf/tgp/plamtv/ui/fragments/RecommendFragment.java
+++ app/src/main/java/com/qf/tgp/plamtv/ui/fragments/RecommendFragment.java
@@ -2,6 +2,7 @@ package com.qf.tgp.plamtv.ui.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,7 @@ public class RecommendFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         layout = inflater.inflate(R.layout.fragment_recommend,container,false);
+        Log.e(TAG, "onCreateView: " );
         return layout;
     }
 }
